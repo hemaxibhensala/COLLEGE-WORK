75e5542e9b4fac69245b11d3b70954aa6c4eb719
hemaxibhensala <121080410+hemaxibhensala@users.noreply.github.com>
2023-02-04 11:58:04 +0530

    UPDATING REPO

--- SEMESTER 2/PYTHON LAB PROGRAMS/Divide the Total amount to 2000,500,and 10 rupees note.py
+++ SEMESTER 2/PYTHON LAB PROGRAMS/Divide the Total amount to 2000,500,and 10 rupees note.py
@@ -3,5 +3,4 @@ note2000=TotalAmount//2000
 rem=TotalAmount%2000
 note500=rem//500
 rem2=rem%500
-note10=rem2//100
-print (note2000,"2000 Rs:",note500,"500 Rs:",note10,"100 Rs:")
+print (note2000,"2000 Rs:",note500,"500 Rs:",rem2,"100 Rs:")
